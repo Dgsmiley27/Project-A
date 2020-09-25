20e3a50985d65c95407da58ca2cc47e169f8b15d
Botnet1 <38007632+Dgsmiley27@users.noreply.github.com>
2020-09-25 11:29:38 -0300

    Update V12

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js')
 module.exports = {
 
   run: async (client, message, args) => {
-    const embed = new Discord.RichEmbed()
-      .setAuthor(`🏓 ${Math.round(client.ping)}ms`)
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(`🏓 ${Math.round(client.ws.ping)}ms`)
       .setColor(message.member ? message.member.displayColor : global.CLIENT_DEFAULT_COLOR)
 
     message.channel.send(embed)
